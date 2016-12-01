7ec7ace1cd0a837ac6a2a9daa4c0a12807708afb
emilycroft <emilyhcroft@gmail.com>
2016-12-01 06:42:32 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    array = self.split(/[.!?]/)
+    array.delete_if { |element| element == "." || element == "?" || element == "!" || element == "" }
+    array.length
   end
 end
